964ed61506bd6aad7c3960cb1a69c2241601fa7d
dewangga-pk <pacetcommunity570@gmail.com>
2025-06-21 05:57:16 +0700

    chore: dashboard & integration create session api

--- frontend/src/components/Cards/ProfileInfoCard.jsx
+++ frontend/src/components/Cards/ProfileInfoCard.jsx
@@ -15,13 +15,13 @@ const ProfileInfoCard = () => {
   return (
     <div className="flex items-center">
       <img
-        src={user.profileImageUrl}
+        src={user?.profileImageUrl}
         alt="Profile Image"
         className="w-11 h-11 bg-gray-300 rounded-full mr-3"
       />
       <div>
         <div className="text-[15px] text-black font-bold leading-3">
-          {user.name || ""}
+          {user?.name || ""}
         </div>
         <button
           className="text-amber-600 text-sm font-semibold cursor-pointer hover:underline"
