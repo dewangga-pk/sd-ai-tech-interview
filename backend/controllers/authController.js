d12137852109f1a340c06adc12a3e8518870d99c
dewangga-pk <pacetcommunity570@gmail.com>
2025-06-21 03:57:58 +0700

    feat: auth integration

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -77,6 +77,11 @@ const getUserProfile = async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
     if (!user){ return res.status(404).json({ message: "User not found"})};
+    
+    if (!user.profileImageUrl) {
+      user.profileImageUrl = `${req.protocol}://${req.get("host")}/uploads/1750393797537-blank_avatar.png`;
+    }
+
     res.json(user);
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message});
